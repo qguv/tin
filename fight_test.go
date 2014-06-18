69bdc1da74a7e1adc98c9b11aa7c74029e53bf41
Cody Kinneer <kinneerc@allegheny.edu>
2014-06-17 22:09:56 -0600

    added debugging output

--- fight_test.go
+++ fight_test.go
@@ -16,16 +16,21 @@ func TestTakeDamage(t *testing.T) {
 		equipedOn: &chest,
 		strength:  80,
 		hardness:  75,
-		dampening: 75,
+		dampening: 50,
 	}
 
 	fmt.Println(test)
 	fmt.Println(chest)
 
+	count := 0
+
 	for chest.health > 0 {
-		test.takeDamage(30, 0, 0)
+		test.takeDamage(75, 75, 15)
 		fmt.Println(test)
 		fmt.Println(chest)
+		count++
 	}
+
+	fmt.Println(count)
 
 }
