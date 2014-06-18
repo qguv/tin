9e00ac0d5fbc8406436af019389a15b565f94456
Cody Kinneer <kinneerc@allegheny.edu>
2014-06-17 19:33:52 -0600

    removed useless printlns

--- characters_test.go
+++ characters_test.go
@@ -11,8 +11,4 @@ func TestSkill(t *testing.T) {
 	rand.Seed(time.Now().UTC().UnixNano())
 	guy := randPerson()
 	fmt.Println(guy.getWeaponSkill())
-	test := sword
-	fmt.Println(test)
-	test = 1
-	fmt.Println(test)
 }
