e4c6d34e4b0bbacb61f0a5d31cdf9af0ae113beb
Quint Guvernator <quintus.public@gmail.com>
2014-06-22 14:16:33 -0600

    menus: differentiate between menu functions and mainmenu-specific code

--- mainmenu.go
+++ mainmenu.go
@@ -2,32 +2,7 @@ package main
 
 import termbox "github.com/nsf/termbox-go"
 import "time"
-import "unicode/utf8"
 import "math/rand"
-
-// putString displays a message horizontally, anchored on its left side by a
-// given coördinate.
-func putString(x, y int, fg, bg termbox.Attribute, msg string) {
-	for _, c := range msg {
-		termbox.SetCell(x, y, c, fg, bg)
-		x++
-	}
-}
-
-// putStringCentered displays a message horizontally, centered by width, at the
-// given height (y).
-func putStringCentered(y int, fg, bg termbox.Attribute, msg string) {
-	w, _ := termbox.Size()
-	lpad := (w - utf8.RuneCountInString(msg)) / 2
-	putString(lpad, y, fg, bg, msg)
-}
-
-// putUserMessage displays a centered message to the user in white at the given
-// height.
-func putUserMessage(m string, h int) {
-	m = smallText(m)
-	putStringCentered(h, termbox.ColorWhite, termbox.ColorDefault, m)
-}
 
 func drawCalibrateReminder() {
 	_, h := termbox.Size()
@@ -160,14 +135,6 @@ func advanceStars(stars []star) {
 		}
 
 		time.Sleep(10 * time.Millisecond)
-	}
-}
-
-func inKeyGroup(k termbox.Key, r rune, keys []termbox.Key, runes []rune) bool {
-	if r == rune(0) {
-		return contains(keys, k)
-	} else {
-		return contains(runes, r)
 	}
 }
 
---
+++ menus.go
@@ -0,0 +1,36 @@
+package main
+
+import termbox "github.com/nsf/termbox-go"
+import "unicode/utf8"
+
+// putString displays a message horizontally, anchored on its left side by a
+// given coördinate.
+func putString(x, y int, fg, bg termbox.Attribute, msg string) {
+	for _, c := range msg {
+		termbox.SetCell(x, y, c, fg, bg)
+		x++
+	}
+}
+
+// putStringCentered displays a message horizontally, centered by width, at the
+// given height (y).
+func putStringCentered(y int, fg, bg termbox.Attribute, msg string) {
+	w, _ := termbox.Size()
+	lpad := (w - utf8.RuneCountInString(msg)) / 2
+	putString(lpad, y, fg, bg, msg)
+}
+
+// putUserMessage displays a centered message to the user in white at the given
+// height.
+func putUserMessage(m string, h int) {
+	m = smallText(m)
+	putStringCentered(h, termbox.ColorWhite, termbox.ColorDefault, m)
+}
+
+func inKeyGroup(k termbox.Key, r rune, keys []termbox.Key, runes []rune) bool {
+	if r == rune(0) {
+		return contains(keys, k)
+	} else {
+		return contains(runes, r)
+	}
+}
