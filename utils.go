dab55662e6110740501c697e2859b8485fc1937c
Quint Guvernator <quintus.public@gmail.com>
2014-06-22 14:01:16 -0600

    utils: comments

--- utils.go
+++ utils.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"math"
-	"strings"
 	"reflect"
+	"strings"
 )
 
+// contains determines whether a slice contains a certain element. Elements of
+// the slice must be of the same type as the testing element.
 func contains(slice_raw interface{}, elem_raw interface{}) bool {
 	elem_type := reflect.TypeOf(elem_raw)
 	if reflect.TypeOf(slice_raw) != reflect.SliceOf(elem_type) {
@@ -22,10 +24,9 @@ func contains(slice_raw interface{}, elem_raw interface{}) bool {
 	return false
 }
 
-func intRoundDiv(num int, divisor int) int {
-	return int((float32(num) / float32(divisor)) + .5)
-}
-
+// stringToLines turns a string (probably a string literal) with an extra
+// newline on both sides into a slice of meaningful lines. Indentation is not
+// ignored.
 func stringToLines(raw string) (out []string) {
 	out = strings.Split(raw, "\n")
 	h := len(out)
@@ -34,6 +35,14 @@ func stringToLines(raw string) (out []string) {
 	return
 }
 
+// intRoundDiv divides two ints as if they were floats and rounds the answer
+// according to arithmetic convention.
+func intRoundDiv(num int, divisor int) int {
+	dividend := float32(num) / float32(divisor)
+	return int(dividend + 0.5)
+}
+
+// percentDiff gives the floating-point absolute difference of two integers.
 func percentDiff(num int, num2 int) float64 {
 	diff := math.Abs(float64(num - num2))
 	avg := float64(num+num2) / 2
