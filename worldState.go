e425dd0301e9103c1b5afafff5c95e70f15c1ac9
Cody Kinneer <kinneerc@allegheny.edu>
2014-06-27 21:38:44 -0600

    added workshops and shop enums

--- worldState.go
+++ worldState.go
@@ -20,12 +20,11 @@ const (
 	haven
 )
 
-type constructionType int
+type manMadeType int
 
 const (
-	// cant use castle, already declared
-	// castle = iota
-	town = iota
+	castleM = iota
+	town
 	village
 	keep
 	port
@@ -43,30 +42,200 @@ type owner interface {
 	getHoldings() []ownable
 }
 
-type construction interface {
+type manmadefeature interface {
 	getOwner() *owner
 	getTile() *tile
-	getType() constructionType
+	getType() manMadeType
 }
 
-type construct struct {
-	name            string
-	owner           *owner
-	construtionType constructionType
-	tile            *tile
+type manmade struct {
+	name        string
+	owner       *owner
+	manMadeType manMadeType
+	tile        *tile
 }
 
-type buildings struct {
-	lumbermill int
-	woodshop   int
-	masonshop  int
-	forge      int
-	smelter    int
+type structureType int
+
+const (
+	admin = iota
+	asylum
+	barrack
+	bath
+	boardingHouse
+	cemetary
+	religious
+	cistern
+	coliseum
+	corral
+	fountain
+	garden
+	granery
+	guildhouse
+	hospital
+	house
+	infirmary
+	inn
+	library
+	mill
+	office
+	plaza
+	prison
+	restaurant
+	shop
+	stable
+	tavern
+	tenemant
+	theater
+	university
+	warehouse
+	well
+	workshop
+)
+
+type workshopType int
+
+const (
+	cobblers         = 87
+	furnatureMakers  = 174
+	furriers         = 240
+	weavers          = 293
+	basketMakers     = 335
+	carpenders       = 377
+	parchmentmakers  = 419
+	potters          = 461
+	wheelwrights     = 499
+	jewlers          = 534
+	masons           = 564
+	bakers           = 594
+	soapmakers       = 620
+	chandlers        = 641
+	coopers          = 661
+	pastryMakers     = 680
+	scabbardMakers   = 695
+	silversmiths     = 710
+	saddlers         = 723
+	purseMakers      = 735
+	blacksmiths      = 747
+	goldsmiths       = 759
+	toymakers        = 771
+	artists          = 782
+	leatherworkers   = 793
+	ropemakers       = 803
+	tanners          = 813
+	buckleMakers     = 822
+	cutlers          = 831
+	fullers          = 840
+	harnessMakers    = 849
+	painters         = 858
+	woodcarvers      = 866
+	glassmakers      = 873
+	instrumentmakers = 880
+	locksmiths       = 887
+	rugMakers        = 894
+	sculptors        = 901
+	shipmakers       = 913
+	bookbinders      = 919
+	fletchers        = 925
+	brewers          = 931
+	gloveMakers      = 937
+	vintner          = 943
+	skinners         = 953
+	armorers         = 958
+	weaponsmiths     = 963
+	distillers       = 967
+	illuminators     = 971
+	perfumer         = 975
+	tilers           = 979
+	potionMakers     = 983
+	clockMakers      = 986
+	taxidermists     = 989
+	vestmentmakers   = 992
+	alchemists       = 994
+	bellmakers       = 996
+	dyeMakers        = 998
+	inventors        = 1000
+)
+
+type shopType int
+
+const (
+	clothiers          = 97
+	gorcers            = 194
+	diarysellers       = 270
+	launderers         = 346
+	prostitutes        = 422
+	furrierMerchants   = 498
+	tailors            = 558
+	barbers            = 607
+	drapers            = 656
+	flowersellers      = 705
+	jewelers           = 745
+	mercers            = 768
+	engravers          = 790
+	pawnbroker         = 812
+	haberdasher        = 832
+	wineMerchants      = 852
+	tinkers            = 868
+	butchers           = 883
+	fishmongers        = 898
+	woolMerchants      = 911
+	beerMerchants      = 923
+	herbalists         = 935
+	spiceMerchants     = 947
+	woodSellers        = 957
+	brothelKeepers     = 965
+	hayMerchants       = 973
+	booksellers        = 979
+	religiousSouvenirs = 985
+	dentists           = 989
+	navelOutfitters    = 993
+	grainMerchants     = 996
+	tobaccoMerchants   = 999
+	magicMerchants     = 1000
+)
+
+//TODO officeType
+
+type buildingStyle int
+
+const (
+	derelict = iota
+	basic
+	ornate
+	imperial
+)
+
+type structure struct {
+	structureType structureType
+	buildingStyle buildingStyle
+}
+
+type districtType int
+
+const (
+	slum = iota
+	gate
+	sea
+	industrial
+	riverD
+	militaryD
+	craftsmen
+	merchantD
+	administrative
+	market
+	patriciate
+)
+
+type district struct {
+	location
+	districtType districtType
+	structures   []structure
 }
 
 type settlement struct {
-	construct
-	buildings
+	manmade
+	districts  []district
 	population []popGroup
 	treasury   int
 }
@@ -89,10 +258,10 @@ type naturalResources struct {
 
 type tile struct {
 	naturalResources
-	claimedBy    *owner
-	location     location
-	biome        biome
-	construction *construction
-	roughness    int
-	wildness     int
+	location
+	claimedBy      *owner
+	biome          biome
+	manmadefeature *manmadefeature
+	roughness      int
+	wildness       int
 }
